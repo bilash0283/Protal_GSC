55e37346ad4d49123e6d31e951cca342ea68606b
bilash0283 <bilash0283@gmail.com>
2024-11-16 02:28:50 -0800

    add new functio on agent add and submit form

--- agent_register.php
+++ agent_register.php
@@ -28,75 +28,75 @@ $successfull = null;
                     <div class="col-12 col-md-6 px-2">
                         <!-- input file start  -->
                         <div class="form-group">
-                            <label for="inputName">Agent Name</label>
+                            <label for="inputName">Agent Name *</label>
                             <input type="text" name="agent_name" id="inputName" class="form-control" required>
                         </div>
                         <div class="form-group">
-                            <label for="inputName"> Phone</label>
+                            <label for="inputName"> Phone *</label>
                             <input type="text" name="phone" id="inputName" class="form-control" required>
                         </div>
                         <div class="form-group">
-                            <label for="inputProjectLeader">Designation</label>
+                            <label for="inputProjectLeader">Designation*</label>
                             <input type="text" name="designation" id="inputProjectLeader" class="form-control" required>
                         </div>
                         <div class="form-group">
-                            <label for="inputProjectLeader">Country</label>
+                            <label for="inputProjectLeader">Country *</label>
                             <input type="text" name="country" id="inputProjectLeader" class="form-control" required>
                         </div>
                         <div class="form-group">
-                            <label for="inputName">Email</label>
+                            <label for="inputName">Email *</label>
                             <input type="email" name="email" id="inputName" class="form-control" required>
                         </div>
                         <div class="form-group">
-                            <label for="inputName">Password</label>
+                            <label for="inputName">Password *</label>
                             <input type="password" name="password" id="inputName" class="form-control" required>
                         </div>
                         <div class="form-group">
-                            <label for="inputName">Confirm Password</label>
+                            <label for="inputName">Confirm Password *</label>
                             <input type="password" name="confirm_password" id="inputName" class="form-control" required>
                         </div>
                         <div class="form-group">
-                            <label class="input-group-text w-100" for="inputGroupFile01">Click Me To Upload Profile Image</label>
+                            <label class="input-group-text w-100" for="inputGroupFile01">Click Me To Upload Profile Image *</label>
                             <input type="file" class="form-control d-none" name="profile_image" id="inputGroupFile01" required>
                         </div>
                         <div class="form-group">
-                            <label for="inputClientCompany"> Company Name</label>
+                            <label for="inputClientCompany"> Company Name *</label>
                             <input type="text" name="company_name" id="inputClientCompany" class="form-control" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="inputProjectLeader">Company Address *</label>
+                            <input type="text" name="company_address" id="inputProjectLeader" class="form-control" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="inputClientCompany">Company Year of Establishment *</label>
+                            <input type="text" name="company_year" id="inputClientCompany" class="form-control" required>
                         </div>
                     </div>  
                     <!-- input file end(1) -->
                     <div class="col-12 col-md-6 px-2">
                         <div class="form-group">
-                            <label for="inputProjectLeader">Company Address</label>
-                            <input type="text" name="company_address" id="inputProjectLeader" class="form-control" required>
-                        </div>
-                        <div class="form-group">
-                            <label for="inputClientCompany">Company Year of Establishment</label>
-                            <input type="text" name="company_year" id="inputClientCompany" class="form-control" required>
-                        </div>
-                        <div class="form-group">
-                            <label for="inputName">Bank Name</label>
+                            <label for="inputName">Bank Name *</label>
                             <input type="text" name="bank_name" id="inputName" class="form-control" required>
                         </div>
                         <div class="form-group">
-                            <label for="inputName">Bank Account Name</label>
+                            <label for="inputName">Bank Account Name *</label>
                             <input type="text" name="bank_acc_name" id="inputName" class="form-control" required>
                         </div>
                         <div class="form-group">
-                            <label for="inputName">Bank Account Number</label>
+                            <label for="inputName">Bank Account Number *</label>
                             <input type="number" name="bank_acc_number" id="inputName" class="form-control" required>
                         </div>
                         <div class="form-group">
-                            <label for="inputName">Bank Address</label>
+                            <label for="inputName">Bank Address *</label>
                             <input type="text" name="bank_address" id="inputName" class="form-control" required>
                         </div>
                         <div class="form-group">
-                            <label for="inputName">Branch Name</label>
+                            <label for="inputName">Branch Name *</label>
                             <input type="text" name="branch_name" id="inputName" class="form-control" required>
                         </div>
                         <div class="form-group">
-                            <label for="inputName">Bank Swift Code</label>
-                            <input type="text" name="swift_code" id="inputName" class="form-control" >
+                            <label for="inputName">Bank Swift Code *</label>
+                            <input type="text" name="swift_code" id="inputName" class="form-control" required>
                         </div>
                         <div class="form-group">
                             <label class="input-group-text w-100" for="inputGroupFile01">Click Me To Upload Company Logo</label>
@@ -106,10 +106,18 @@ $successfull = null;
                             <label class="input-group-text w-100" for="inputGroupFile01">Company Registration Certificate</label>
                             <input type="file" class="form-control d-none" name="company_reg_certificate" id="inputGroupFile01">
                         </div>
+                        <div class="form-group">
+                            <label for="inputProjectLeader">Facebook URL</label>
+                            <input type="url" name="fb_url" id="inputProjectLeader" class="form-control" >
+                        </div>
+                        <div class="form-group">
+                            <label for="inputClientCompany">Website URL</label>
+                            <input type="url" name="website_url" id="inputClientCompany" class="form-control" >
+                        </div>
                         <p class="mb-0">
                             <a href="index.php" class="text-center">I already have an Agent</a>
                         </p>
-                        <button type="submit" name="submit" class="btn btn-primary btn-arrow px-4 mt-3">
+                        <button type="submit" name="submit" class="btn btn-primary btn-block btn-arrow px-4 mt-3">
                             Register Now <i class="fas fa-arrow-right ml-3"></i>
                         </button>
                     </div>
@@ -123,136 +131,150 @@ $successfull = null;
 
 <!-- form sumbmication  -->
 <?php
+// Query to get all emails from the database
 $agent_mail_query = "SELECT email FROM agents";
 $emailquery = mysqli_query($db, $agent_mail_query);
-$res = mysqli_num_rows($emailquery);
-
+$existing_emails = [];
 while ($row = mysqli_fetch_assoc($emailquery)) {
-$query_email = $row['email'];
-};
+    $existing_emails[] = $row['email'];
+}
 
+// Check if form is submitted
 if (isset($_POST['submit'])) {
 
     // Collecting form data
-    $agent_name                = $_POST['agent_name'];
-    $phone                     = $_POST['phone'];
-    $designation               = $_POST['designation'];
-    $country                   = $_POST['country'];
-    $email                     = $_POST['email'];
-    $password                  = md5($_POST['password']);
-    $confirm_password          = md5($_POST['confirm_password']);
-    $company_name              = $_POST['company_name'];
-    $company_address           = $_POST['company_address'];
-    $company_year              = $_POST['company_year'];
-    $bank_name                 = $_POST['bank_name'];
-    $bank_acc_name             = $_POST['bank_acc_name'];
-    $bank_acc_number           = $_POST['bank_acc_number'];
-    $bank_address              = $_POST['bank_address'];
-    $branch_name               = $_POST['branch_name'];
-    $swift_code                = $_POST['swift_code'];
-    $status                    = '2';
-    $role                      = '2';
-    $profile_image             = $_FILES['profile_image'];
-    $company_logo_img          = $_FILES['company_logo_img'];
-    $company_reg_certificate   = $_FILES['company_reg_certificate'];
-   
+    $agent_name = $_POST['agent_name'];
+    $phone = $_POST['phone'];
+    $designation = $_POST['designation'];
+    $country = $_POST['country'];
+    $email = $_POST['email'];
+    $password = md5($_POST['password']);
+    $confirm_password = md5($_POST['confirm_password']);
+    $company_name = $_POST['company_name'];
+    $company_address = $_POST['company_address'];
+    $company_year = $_POST['company_year'];
+    $bank_name = $_POST['bank_name'];
+    $bank_acc_name = $_POST['bank_acc_name'];
+    $bank_acc_number = $_POST['bank_acc_number'];
+    $bank_address = $_POST['bank_address'];
+    $branch_name = $_POST['branch_name'];
+    $swift_code = $_POST['swift_code'];
+    $status = '2'; // default value
+    $role = '2'; // default value
+    $fb_url = $_POST['fb_url'];
+    $website_url = $_POST['website_url'];
     
+    // Handle file uploads
+    $profile_image = $_FILES['profile_image']['name'];
+    $company_logo_img = $_FILES['company_logo_img']['name'];
+    $company_reg_certificate = $_FILES['company_reg_certificate']['name'];
 
-$profile_image            = $_FILES['profile_image']['name'];
-$temporary_location       = $_FILES['profile_image']['tmp_name'];
+    // Temporary file locations
+    $profile_temp = $_FILES['profile_image']['tmp_name'];
+    $logo_temp = $_FILES['company_logo_img']['tmp_name'];
+    $cert_temp = $_FILES['company_reg_certificate']['tmp_name'];
 
-$company_logo_img         = $_FILES['company_logo_img']['name'];
-$temporary_location       = $_FILES['company_logo_img']['tmp_name'];
+    // Error array
+    $errors = [];
 
-$company_reg_certificate  = $_FILES['company_reg_certificate']['name'];
-$temporary_location       = $_FILES['company_reg_certificate']['tmp_name'];
+    // Validate image files
+    if (!empty($profile_image) || !empty($company_logo_img) || !empty($company_reg_certificate)) {
+        $allowed_extensions = ['jpg', 'jpeg', 'png', 'gif'];
 
+        // Check profile image
+        if (!empty($profile_image)) {
+            $profile_extension = strtolower(pathinfo($profile_image, PATHINFO_EXTENSION));
+            if (!in_array($profile_extension, $allowed_extensions)) {
+                $errors[] = "Profile image should be JPG, JPEG, PNG, or GIF.";
+            }
+            if ($_FILES['profile_image']['size'] > 2 * 1024 * 1024) {
+                $errors[] = "Profile image size should not exceed 2MB.";
+            }
+        }
 
-$errors = [];
+        // Check company logo image
+        if (!empty($company_logo_img)) {
+            $logo_extension = strtolower(pathinfo($company_logo_img, PATHINFO_EXTENSION));
+            if (!in_array($logo_extension, $allowed_extensions)) {
+                $errors[] = "Company logo image should be JPG, JPEG, PNG, or GIF.";
+            }
+            if ($_FILES['company_logo_img']['size'] > 2 * 1024 * 1024) {
+                $errors[] = "Company logo image size should not exceed 2MB.";
+            }
+        }
 
-// 5. Validate image file (if uploaded)
-if (!empty($profile_image) && !empty($company_logo_img) && !empty($company_logo_img)) {
-$allowed_extensions = ['jpg', 'jpeg', 'png', 'gif'];
-$image_extension = strtolower(pathinfo($image, PATHINFO_EXTENSION));
+        // Check company registration certificate
+        if (!empty($company_reg_certificate)) {
+            $cert_extension = strtolower(pathinfo($company_reg_certificate, PATHINFO_EXTENSION));
+            if (!in_array($cert_extension, $allowed_extensions)) {
+                $errors[] = "Company registration certificate image should be JPG, JPEG, PNG, or GIF.";
+            }
+            if ($_FILES['company_reg_certificate']['size'] > 2 * 1024 * 1024) {
+                $errors[] = "Company registration certificate size should not exceed 2MB.";
+            }
+        }
+    }
 
-// Check if the file is an allowed image type
-if (!in_array($image_extension, $allowed_extensions)) {
-$errors[] = "Only JPG, JPEG, PNG, and GIF images are allowed.";
-}
+    // Check if email already exists
+    if (in_array($email, $existing_emails)) {
+        $errors[] = "This email is already registered.";
+    }
 
-// Check image size (max 2MB)
-if ($_FILES['image']['size'] > 2 * 1024 * 1024) {
-$errors[] = "Image size should not exceed 2MB.";
-}
-if ($query_email == $email) {
-$errors[] = "This email is alerady Existed";
-}
-}
+    // Password validation
+    if ($password !== $confirm_password) {
+        $errors[] = "Passwords do not match.";
+    }
 
-// If there are errors, display them and stop the insertion process
-if (!empty($errors)) {
-foreach ($errors as $error) {
-echo "<div class='alert alert-danger mt-2'>$error</div>";
-}
-} else {
-// If no errors, process the data and insert into the database
-if ($password == $confirm_password) {
+    // If no errors, insert data into the database
+    if (empty($errors)) {
 
-if ($query_email !== $email) {
+        // Generate unique filenames for uploaded files
+        $rand = rand(0, 999999);
+        $final_profile_image = time() . "_" . $rand . "_" . $profile_image;
+        $final_logo_image = time() . "_" . $rand . "_" . $company_logo_img;
+        $final_cert_image = time() . "_" . $rand . "_" . $company_reg_certificate;
 
-if (!empty($image)) {
-    // Generate a unique image name
-    $rand = rand(0, 999999);
-    $final_image_profile    = $name . "_" . $rand . time() . $image;
-    $final_company_logo_img = $name . "_" . $rand . time() . $image;
-    $final_company_logo_img = $name . "_" . $rand . time() . $image;
-    
+        // Move uploaded files to their respective directories
+        if (!empty($profile_image)) {
+            move_uploaded_file($profile_temp, 'dist/img/agent_image/' . $final_profile_image);
+        }
+        if (!empty($company_logo_img)) {
+            move_uploaded_file($logo_temp, 'dist/img/agent_company_logo/' . $final_logo_image);
+        }
+        if (!empty($company_reg_certificate)) {
+            move_uploaded_file($cert_temp, 'dist/img/agent_registation_cartificate/' . $final_cert_image);
+        }
 
-    // Move the uploaded image to the target directory
-    move_uploaded_file($temporary_location, 'dist/img/agent_image/' . $final_image_profile);
-    move_uploaded_file($temporary_location, 'dist/img/agent_company_logo/' . $final_company_logo_img);
-    move_uploaded_file($temporary_location, 'dist/img/agent_registation_cartificate/' . $final_company_logo_img);
+        // Insert data into the database
+        $agent_insert = "INSERT INTO agents 
+        (name, email, password, phone, company, designation, year, country, role, status, address, image, joining, bank_name, bank_acc_name, bank_acc_number, bank_address, branch_name, swift_code, company_logo, company_reg_cert, fb_url, web_url) 
+        VALUES 
+        ('$agent_name', '$email', '$password', '$phone', '$company_name', '$designation', '$company_year', '$country', '$role', '$status', '$company_address', '$final_profile_image', NOW(), '$bank_name', '$bank_acc_name', '$bank_acc_number', '$bank_address', '$branch_name', '$swift_code', '$final_logo_image', '$final_cert_image', '$fb_url', '$website_url')";
 
-    // Insert data into the database
-    $agent_insert = "INSERT INTO agents (name, email, password, phone, company, designation, year, 
-    country, role, status, address, image, joining) VALUES('$name', '$email', '$password',
-    '$phone', '$company', '$designation', '$year', '$country', '$role', '$status', '$address',
-    '$final_image_name', now())";
-} else {
-    // If no image uploaded, insert data without image
-    $agent_insert = "INSERT INTO agents (name, email, password, phone, company, designation, year, 
-    country, role, status, address, joining) VALUES('$name', '$email', '$password',
-    '$phone', '$company', '$designation', '$year', '$country', '$role', '$status', '$address',
-    now())";
-}
+        $agent_sql = mysqli_query($db, $agent_insert);
 
-// Execute the query
-$agent_sql = mysqli_query($db, $agent_insert);
+        // Check if the insertion was successful
+        if ($agent_sql) {
+            header("Location: " . $_SERVER['PHP_SELF'] . "?success=1");
+            exit();
+        } else {
+            echo "<div class='alert alert-danger mt-2'>An error occurred while registering the agent. Please try again.</div>";
+        }
 
-// Check if the insertion was successful
-if ($agent_sql) {
-    
-    header("Location: " . $_SERVER['PHP_SELF'] . "?success=1");
-    exit();
-
-    
-} else {
-    echo "<div class='alert alert-danger mt-2'>An error occurred while registering the agent. Please try again.</div>";
-}
-} else {
-$errors[] = "This email is alerady Existed";
-}
-}
-}
+    } else {
+        // Display errors
+        foreach ($errors as $error) {
+            echo "<div class='alert alert-danger mt-2'>$error</div>";
+        }
+    }
 }
 ?>
+
+<!--  Form Submission -->
 
 
 
 <?php 
-// Form Submission Code 
-
-
 ob_end_flush();
 include('login_include/footer.php'); 
 ?>
