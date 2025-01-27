9896893ae59993077af65ed1bd4e3ff8e8cf102a
bilash0283 <bilash0283@gmail.com>
2025-01-27 15:43:24 +0600

    studen and agent page pagniction add

--- leed_student_show.php
+++ leed_student_show.php
@@ -1,200 +1,126 @@
-<?php include('dashboard_include/header.php') ?>
-<?php ob_start(); ?>
-  <!-- Navbar -->
-  <?php include('dashboard_include/top_header.php') ?>
-  <!-- /.navbar -->
-  <!-- Main Sidebar Container -->
-  <?php include('dashboard_include/sidebar.php')?>
+<?php
+include('dashboard_include/header.php');
+ob_start();
+include('dashboard_include/top_header.php');
+include('dashboard_include/sidebar.php');
 
-  <?php
+if ($_SESSION['role'] == 1 || $_SESSION['role'] == 3) { ?>
 
-    if ($_SESSION['role'] == 1 || $_SESSION['role'] == 3) { ?>
-        <!-- Content Wrapper. Contains page content -->
-      <div class="content-wrapper">
-        <!-- Content Header (Page header) -->
-        <div class="content-header">
-          <div class="container-fluid">
+<div class="content-wrapper">
+    <div class="content-header">
+        <div class="container-fluid">
             <div class="row mb-2">
-              <div class="col-sm-6">
-                <h1 class="m-0">All New Student</h1>
-              </div><!-- /.col -->
-              <div class="col-sm-6">
-                <ol class="breadcrumb float-sm-right">
-                  <li class="breadcrumb-item"><a href="#">Home</a></li>
-                  <li class="breadcrumb-item active">Dashboard v1</li>
-                </ol>
-              </div><!-- /.col -->
-            </div><!-- /.row -->
-          </div><!-- /.container-fluid -->
-        </div>
-        <!-- /.content-header -->
-        <!-- Main content -->
-        <section class="content">
-          <div class="container-fluid">
-          <div class="row">
-            <div class="col-md-12">
-                <table class="table">
-                  <thead>
-                    <tr>
-                      <th scope="col">View Details</th>
-                      <th scope="col">Name</th>
-                      <th scope="col">Phone</th>
-                      <th scope="col">Status</th>
-                      <th scope="col">Role</th>
-                      <?php 
-                        if ($_SESSION['role'] == 1) { ?>
-                          <th scope="col">Action</th>
-                      <?php  }
-                      ?>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <?php
-                      $agents = "SELECT * FROM agents WHERE role='4' ORDER BY id DESC";
-                      $agents_query = mysqli_query($db, $agents);
-                      $count = mysqli_num_rows($agents_query);
-
-                      if ($count < 1) {
-                        echo "There are no Active Agents!!";
-                      } else {
-                        while ($row = mysqli_fetch_assoc($agents_query)) {
-                          $id           = $row['id'];
-                          $name         = $row['name'];
-                          $email        = $row['email'];
-                          $phone        = $row['phone'];
-                          $designation  = $row['designation'];
-                          $status       = $row['status'];
-                          $role         = $row['role'];
-                          ?>
-
-                          <tr>
-                          <td><?php ?>
-
-                          <?php 
-                          
-                          $st_id_sql = "SELECT * FROM newstudents WHERE email='$email' ";
-                          $st_id_qu = mysqli_query($db,$st_id_sql);
-                          while($row = mysqli_fetch_assoc($st_id_qu)){
-                            $edit_stid = $row['id'];
-                          }
-                          
-                          ?>
-                            
-                            <a href="stu_single_view.php?edit=<?php echo $edit_stid; ?>"><i class="fas fa-eye"></i></a>
-                            <?php 
-                            ?></td>
-                            <td><?php echo $name; ?></td>
-                            <td><?php echo $phone; ?></td>
-                            <td>
-                              <?php
-                              
-                              if ($status == 1) {
-                                echo "<div class='badge bg-success' >Check</div>";
-                              } else {
-                                echo "<div class='badge bg-secondary' >Uncheck</div>";
-                              }
-
-                              ?>
-                            </td>
-                            <td>
-                            <?php
-                              
-                              if ($role == 1) {
-                                echo "<div class='badge bg-success' >Admin</div>";
-                              } else if ($role == 2){
-                                echo "<div class='badge bg-warning' >Agent</div>";
-                              } else if($role == 3){
-                                echo "<div class='badge bg-info' >Employee</div>";
-                              }else if($role == 4){
-                                echo "<div class='badge bg-info' >Student</div>";
-                              }
-
-                              ?>
-                            </td>
-
-                              <?php 
-                              
-                              if ($_SESSION['role'] == 1) { ?>
-                                <td>
-
-                                <div class="btn-group">
-                                  <a href="std_status_update.php?edit=<?php echo $id?>" class="btn btn-primary btn-sm">Edit</a>
-                                  <a href="leed_student_show.php?delemail=<?php echo $email; ?>" class="btn btn-danger btn-sm" onclick="console.log('Deleting data...'); return confirm('Are you sure you want to delete this data?');">Delete</a>
-
-                                </div>
-  
-                              </td>
-                             <?php }
-                              
-                              ?>
-
-                            <div class="modal fade" id="id<?php echo $email?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                              <div class="modal-dialog">
-                                <div class="modal-content">
-                                  <div class="modal-header">
-                                    <h5 class="modal-title" id="exampleModalLabel">Delete Student</h5>
-                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                      <span aria-hidden="true">&times;</span>
-                                    </button>
-                                  </div>
-                                  <div class="modal-body">
-                                    <p>Are you sure to delete <strong><?php echo $email?></strong></p>
-                                  </div>
-                                  <div class="modal-footer">
-                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                    <a href="" class="btn btn-primary">Delete Student</a>
-                                  </div>
-                                </div>
-                              </div>
-                            </div>
-                          </tr>
-                          <?php
-                        }
-                      }
-                    ?>
-                  </tbody>
-                </table>
+                <div class="col-sm-6">
+                    <h1 class="m-0">All New Students</h1>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item active">Dashboard</li>
+                    </ol>
+                </div>
             </div>
-          </div>
+        </div>
+    </div>
 
-            <?php
-              if(isset($_GET['delemail'])){
-                $delete_email = $_GET['delemail'];
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">View Details</th>
+                                <th scope="col">Name</th>
+                                <th scope="col">Phone</th>
+                                <th scope="col">Status</th>
+                                <th scope="col">Role</th>
+                                <?php if ($_SESSION['role'] == 1) { ?>
+                                    <th scope="col">Action</th>
+                                <?php } ?>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            $rows_per_page = 6; // Number of rows per page
+                            $current_page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
+                            $offset = ($current_page - 1) * $rows_per_page;
 
-                $stu_del = "DELETE FROM newstudents WHERE email = '$delete_email'";
-                $stu_que = mysqli_query($db,$stu_del);
+                            // Query to fetch limited rows with pagination
+                            $agents = "SELECT * FROM agents WHERE role='4' ORDER BY id DESC LIMIT $offset, $rows_per_page";
+                            $agents_query = mysqli_query($db, $agents);
 
-                $delete_sql = "DELETE FROM agents WHERE email = '$delete_email'";
-                $delete = mysqli_query($db,$delete_sql);
-                if($delete && $stu_que){
-                  header('location:leed_student_show.php');
-                  unset($_GET['delemail']);
-                } else {
-                  echo "<div class='alert alert-danger mt-2'>An Error Occured While Deleting!</div>";
-                }
-              }
-            ?>
+                            $total_rows_query = mysqli_query($db, "SELECT COUNT(*) as total FROM agents WHERE role='4'");
+                            $total_rows = mysqli_fetch_assoc($total_rows_query)['total'];
+                            $total_pages = ceil($total_rows / $rows_per_page);
 
-            
-          </div><!-- /.container-fluid -->
-        </section>
-        <!-- /.content -->
-      </div>
-      <!-- /.content-wrapper -->
-    <?php } else {
-      echo "<div class='alert alert-danger mt-2 text-center'><h1>Only Admin Allowed!</h1></div>";
-    }
+                            if (mysqli_num_rows($agents_query) < 1) {
+                                echo "<tr><td colspan='6'>No Active Agents Found!</td></tr>";
+                            } else {
+                                while ($row = mysqli_fetch_assoc($agents_query)) {
+                                    $id = $row['id'];
+                                    $name = $row['name'];
+                                    $email = $row['email'];
+                                    $phone = $row['phone'];
+                                    $status = $row['status'];
+                                    $role = $row['role']; ?>
 
-  ?>
+                                    <tr>
+                                        <td><a href="stu_single_view.php?edit=<?php echo $id; ?>"><i class="fas fa-eye"></i></a></td>
+                                        <td><?php echo $name; ?></td>
+                                        <td><?php echo $phone; ?></td>
+                                        <td>
+                                            <?php echo $status == 1 ? "<div class='badge bg-success'>Active</div>" : "<div class='badge bg-secondary'>Inactive</div>"; ?>
+                                        </td>
+                                        <td>
+                                            <?php
+                                            if ($role == 1) echo "<div class='badge bg-success'>Admin</div>";
+                                            elseif ($role == 2) echo "<div class='badge bg-warning'>Agent</div>";
+                                            elseif ($role == 3) echo "<div class='badge bg-info'>Employee</div>";
+                                            elseif ($role == 4) echo "<div class='badge bg-info'>Student</div>";
+                                            ?>
+                                        </td>
+                                        <?php if ($_SESSION['role'] == 1) { ?>
+                                            <td>
+                                                <div class="btn-group">
+                                                    <a href="std_status_update.php?edit=<?php echo $id ?>" class="btn btn-primary btn-sm">Edit</a>
+                                                    <a href="leed_student_show.php?delemail=<?php echo $email; ?>" class="btn btn-danger btn-sm" onclick="return confirm('Are you sure you want to delete this data?');">Delete</a>
+                                                </div>
+                                            </td>
+                                        <?php } ?>
+                                    </tr>
+                                <?php }
+                            } ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
 
+            <!-- Pagination -->
+            <nav aria-label="Page navigation example">
+                <ul class="pagination justify-content-center">
+                    <?php if ($current_page > 1) { ?>
+                        <li class="page-item"><a class="page-link" href="?page=<?php echo $current_page - 1; ?>">Previous</a></li>
+                    <?php } ?>
 
-  
+                    <?php for ($i = 1; $i <= $total_pages; $i++) { ?>
+                        <li class="page-item <?php echo $i == $current_page ? 'active' : ''; ?>">
+                            <a class="page-link" href="?page=<?php echo $i; ?>"><?php echo $i; ?></a>
+                        </li>
+                    <?php } ?>
 
-  <?php
-    ob_end_flush();
-   ?>
+                    <?php if ($current_page < $total_pages) { ?>
+                        <li class="page-item"><a class="page-link" href="?page=<?php echo $current_page + 1; ?>">Next</a></li>
+                    <?php } ?>
+                </ul>
+            </nav>
+        </div>
+    </section>
+</div>
+<?php } else {
+    echo "<div class='alert alert-danger text-center'><h1>Only Admin Allowed!</h1></div>";
+}
 
-  <!-- /.main-footer -->
-   <?php include('dashboard_include/footer.php') ?>
-
-  
+include('dashboard_include/footer.php');
+ob_end_flush();
+?>
